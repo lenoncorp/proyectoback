a4b882d77c6da70e20865cc8483b0e812100194c
fredyrios22 <fredy.rios@udea.edu.co>
2021-12-08 00:47:18 -0500

    2021-12-08 se agregan cambios para el modelo de inscripciones

--- models/inscripcion/tipos.js
+++ models/inscripcion/tipos.js
@@ -17,7 +17,6 @@ const tiposInscripcion = gql`
 
     type Mutation {
         crearInscripcion(
-        estado: Enum_EstadoInscripcion!
         proyecto: String!
         estudiante: String!
     ): Inscripcion
